3006d1cecc063b31e48814f9bfa3c2535e78d3a0
Richard Carlsson <carlsson.richard@gmail.com>
2010-07-18 14:49:38 +0200

    some initial setup for using yajl

--- chapter_12/plain_port/json_parser/c_src/parser.c
+++ chapter_12/plain_port/json_parser/c_src/parser.c
@@ -1,9 +1,14 @@
 #include "erl_interface.h"
 #include "ei.h"
 
-
+#include <yajl/yajl_parse.h>
+#include <yajl/yajl_gen.h>
 
 int main(int argc, char **argv) {
+    yajl_gen g;
+    yajl_gen_config conf = { 1, "  " };
+    g = yajl_gen_alloc(&conf, NULL);
+
     erl_init(NULL, 0);
 
     return 0;
