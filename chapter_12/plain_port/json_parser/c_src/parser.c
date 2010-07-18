b90f6a8c2cf3b5da8a10cad37fe7223952264d8b
Richard Carlsson <carlsson.richard@gmail.com>
2010-07-19 00:38:40 +0200

    actually working now

--- chapter_12/plain_port/json_parser/c_src/parser.c
+++ chapter_12/plain_port/json_parser/c_src/parser.c
@@ -60,119 +60,259 @@ static yajl_alloc_funcs alloc_funcs = {
 };
 */
 
+
+struct container {
+  int index;    /* offset of container header */
+  int count;    /* number of elements */
+  struct container *next;
+};
+
+struct state {
+  ei_x_buff x;         /* the dynamic buffer */
+  struct container *c; /* innermost container */
+};
+
+
+size_t read_bytes(unsigned char *buf, size_t max, FILE *fd)
+{
+  size_t n;
+  n = fread(buf, 1, max, fd);
+  if ((n == 0) && !feof(fd)) {
+    fprintf(stderr, "read error\n");
+    exit(1);
+  }
+  buf[n] = 0; /* zero-terminate the read data */
+  return n;
+}
+
+static void write_packet(ei_x_buff *x, FILE *fd)
+{
+  uint8_t hd[4];
+  int sz = x->buffsz;
+  
+  /* the packet header must be in Network Byte Order */
+  hd[0] = (sz >> 24) & 0xff;
+  hd[1] = (sz >> 16) & 0xff;
+  hd[2] = (sz >> 8) & 0xff;
+  hd[3] = sz & 0xff;
+  fwrite(hd, 1, 4, fd);
+
+  fwrite(x->buff, 1, sz, fd);
+  fflush(fd);
+}
+
+/* returns an ETERM pointer to a binary term, or NULL */
+static ETERM *read_document(unsigned char *buf, size_t max, FILE *fd)
+{
+  size_t n, sz;
+  uint8_t hd[4];
+  ETERM *t;
+  
+  n = read_bytes(hd, 4, fd);
+  if (n != 4) {
+    if (n == 0 && feof(fd)) return NULL;
+    fprintf(stderr, "error reading packet header\n");
+    exit(1);
+  }
+  /* the packet header is always in Network Byte Order */
+  sz = (hd[0] << 24) + (hd[1] << 16) + (hd[2] << 8) + hd[3];
+  if (sz > max) {
+    fprintf(stderr, "packet too big (%d bytes)\n", sz);
+    exit(1);
+  }
+  n = read_bytes(buf, sz, stdin);
+  if (n != sz) {
+    fprintf(stderr, "error reading %d bytes; got %d\n", sz, n);
+    exit(1);
+  }
+  t = erl_decode(buf);
+  if (t == NULL) {
+    fprintf(stderr, "could not decode document\n");
+    exit(1);
+  }
+  if (!ERL_IS_BINARY(t)) {
+    fprintf(stderr, "document is not a binary\n");
+    exit(1);
+  }
+  return t;
+}
+
+void write_error(const char *text)
+{
+  ei_x_buff x;
+  ei_x_new_with_version(&x);
+  ei_x_encode_tuple_header(&x, 2);
+  ei_x_encode_atom(&x, "error");
+  ei_x_encode_string(&x, text);
+  write_packet(&x, stdout);
+  ei_x_free(&x);
+}
+
+
 int main(int argc, char **argv)
 {
   erl_init(NULL, 0); /* initialize erl_interface */
-  
+  ETERM *t;
+  struct state st;
+
   yajl_parser_config cfg = {
     1, /* allow comments */
     0  /* don't check UTF-8 */
   };
   yajl_handle hand;
   yajl_status stat;
-  static unsigned char fileData[65536];
-  int done = 0;
-  size_t rd;
+  static unsigned char buf[65536];
 
-  /* hand = yajl_alloc(&callbacks, &cfg, &alloc_funcs, NULL); */
-  hand = yajl_alloc(&callbacks, &cfg, NULL, NULL);
-  while (!done) {
-    rd = fread((void *) fileData, 1, sizeof(fileData) - 1, stdin);
-    fileData[rd] = 0; /* zero-terminate the read data */
-    if (rd == 0) {
-      if (!feof(stdin)) {
-        fprintf(stderr, "read error\n");
-        exit(1);
-      }
-      done = 1;
-    }
-    if (done) {
+  while ((t = read_document(buf, sizeof(buf) - 1, stdin)) != NULL) {
+    /* initialize the state and the output buffer */
+    st.c = NULL;
+    ei_x_new_with_version(&st.x);
+    ei_x_encode_tuple_header(&st.x, 2);
+    ei_x_encode_atom(&st.x, "ok");
+    hand = yajl_alloc(&callbacks, &cfg, NULL, &st);
+    stat = yajl_parse(hand, ERL_BIN_PTR(t), ERL_BIN_SIZE(t));
+    if (stat == yajl_status_insufficient_data) {
       stat = yajl_parse_complete(hand);
-    } else {
-      stat = yajl_parse(hand, fileData, rd);
     }
-    if ((stat != yajl_status_ok)
-        && (stat != yajl_status_insufficient_data)) {
-      unsigned char *str = yajl_get_error(hand, 1, fileData, rd);
-      fprintf(stderr, "%s\n", (const char *)str);
+    if (stat == yajl_status_insufficient_data) {
+      write_error("unexpected end of document");
+    } else if (stat != yajl_status_ok) {
+      unsigned char *str = yajl_get_error(hand, 0, NULL, 0);
+      write_error(str);
       yajl_free_error(hand, str);
+    } else {
+      write_packet(&st.x, stdout);
     }
+    yajl_free(hand);
+    ei_x_free(&st.x);
+    erl_free(t);
   }
-  yajl_free(hand);
-
   return 0;
 }
 
 
+/*
+ * JSON                            Erlang json() representation
+ * ----                            ----------------------------
+ * true                            'true'
+ * false                           'false'
+ * null                            'undefined'
+ * number (integers and floats)    number()
+ * string: "..."                   binary()
+ * array/list: [ value, ... ]      {json()}
+ * map: { label: value, ... }      [{binary(), json()}]
+ */
+
+static void count_element(struct state *st)
+{
+  struct container *c = st->c;
+  if (c != NULL) ++(c->count);
+}
+
 static int handle_null(void * ctx)
 {
-  /* TODO */
-  printf("Null.\n");
+  struct state *st = (struct state *)ctx;
+  count_element(st);
+  ei_x_encode_atom(&st->x, "undefined");
   return 1;
 }
 
 static int handle_boolean(void * ctx, int boolean)
 {
-  /* TODO */
-  printf("Boolean: %d\n", boolean);
+  struct state *st = (struct state *)ctx;
+  count_element(st);
+  ei_x_encode_boolean(&st->x, boolean);
   return 1;
 }
 
 static int handle_integer(void * ctx, long integerVal)
 {
-  /* TODO */
-  printf("Integer: %ld\n", integerVal);
+  struct state *st = (struct state *)ctx;
+  count_element(st);
+  ei_x_encode_long(&st->x, integerVal);
   return 1;
 }
 
 static int handle_double(void * ctx, double doubleVal)
 {
-  /* TODO */
-  printf("Double: %f\n", doubleVal);
+  struct state *st = (struct state *)ctx;
+  count_element(st);
+  ei_x_encode_double(&st->x, doubleVal);
   return 1;
 }
 
 static int handle_string(void * ctx, const unsigned char * stringVal,
                          unsigned int stringLen)
 {
-  /* TODO */
-  printf("String: '%.*s'\n", stringLen, stringVal);
+  struct state *st = (struct state *)ctx;
+  count_element(st);
+  ei_x_encode_binary(&st->x, stringVal, stringLen);
   return 1;
 }
 
 static int handle_map_key(void * ctx, const unsigned char * stringVal,
                           unsigned int stringLen)
 {
-  /* TODO */
-  printf("Map Key: '%.*s'\n", stringLen, stringVal);
+  struct state *st = (struct state *)ctx;
+  /* Begin a 2-tuple with the key string as first element.
+     The next called callback function will encode the value.
+     Don't count the keys, only the values. */
+  ei_x_encode_tuple_header(&st->x, 2);
+  ei_x_encode_binary(&st->x, stringVal, stringLen);
+  return 1;
+}
+
+static int handle_start(void * ctx, int array)
+{
+  struct state *st = (struct state *)ctx;
+  struct container *c = malloc(sizeof(struct container));
+  count_element(st);
+  c->next = st->c;
+  st->c = c;
+  c->count = 0;
+  c->index = st->x.index; /* save current position */
+  /* write a dummy header for now */
+  if (array) {
+    ei_x_encode_tuple_header(&st->x, 1);
+  } else {
+    ei_x_encode_list_header(&st->x, 1);
+  }
   return 1;
 }
 
 static int handle_start_map(void * ctx)
 {
-  /* TODO */
-  printf("Start map.\n");
-  return 1;
-}
-
-
-static int handle_end_map(void * ctx)
-{
-  /* TODO */
-  printf("End map.\n");
-  return 1;
+  handle_start(ctx, 0);
 }
 
 static int handle_start_array(void * ctx)
 {
-  /* TODO */
-  printf("Start array.\n");
+  handle_start(ctx, 1);
+}
+
+static int handle_end(void * ctx, int array)
+{
+  struct state *st = (struct state *)ctx;
+  struct container *c = st->c;
+  int index = st->c->index; /* temporary variable needed */
+  /* back-patch the header */
+  if (array) {
+    ei_encode_tuple_header(st->x.buff, &index, st->c->count);
+  } else {
+    ei_encode_list_header(st->x.buff, &index, st->c->count);
+    ei_x_encode_empty_list(&st->x);  /* also terminate the list */
+  }
+  st->c = c->next;
+  free(c);
   return 1;
+}
+
+static int handle_end_map(void * ctx)
+{
+  handle_end(ctx, 0);
 }
 
 static int handle_end_array(void * ctx)
 {
-  /* TODO */
-  printf("End array.\n");
-  return 1;
+  handle_end(ctx, 1);
 }
