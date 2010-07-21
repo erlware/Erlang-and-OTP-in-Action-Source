36e8b7ee701e7ae358db64a9d50d65f0bdecea14
Richard Carlsson <carlsson.richard@gmail.com>
2010-07-21 23:12:49 +0200

    removed handling of io-lists in frontend to keep nif version simpler

--- chapter_12/port_driver/json_parser/src/json_parser.erl
+++ chapter_12/port_driver/json_parser/src/json_parser.erl
@@ -7,6 +7,6 @@
 
 -export([parse_document/1]).
 
-%% @doc Parses a document that can be an io-list
+%% @doc Parses a document given as a binary
 parse_document(Data) ->
-    jp_server:parse_document(iolist_to_binary(Data)).
+    jp_server:parse_document(Data).
